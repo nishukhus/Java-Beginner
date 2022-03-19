e3c2b79ff203c6d08ffe914f246a6ccd85567ca2
nishukhus <99001064+nishukhus@users.noreply.github.com>
2022-03-19 09:07:50 +0530

    Add files via upload

---
+++ Assigment_2.java
@@ -0,0 +1,13 @@
+public class Assigment_2 {
+
+    public static void main(String[] args) {
+        // Syntax : main Method after that use println code for print something
+
+        System.out.println("Hi This is Neeshu Bhadauriya");
+        System.out.println("Hi This is Khushu");
+        System.out.println("Hi This is Pucchu Laal");
+        System.out.println("Printing JAVA");
+        System.out.println("Java is Coding Language");
+        System.out.println("I m using HP");
+    }
+}
---
+++ DemoDay1.java
@@ -0,0 +1,16 @@
+public class DemoDay1 {
+
+
+    public static void main(String [] args){
+
+      System.out.println("This is my first java Program");
+
+      System.out.println("Hello");
+      System.out.println("Test");
+
+    }
+
+
+
+}
+
